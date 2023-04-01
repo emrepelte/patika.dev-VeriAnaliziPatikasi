28c03f6e03a12bc2dd86baa06fdf4092b1b6c7b3
emrepelte <124101351+emrepelte@users.noreply.github.com>
2023-04-01 17:15:40 +0300

    Create BinarySearchTreeProjesi.py

---
+++ BinarySearchTreeProjesi.py
@@ -0,0 +1,27 @@
+# # Proje 3
+# [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+# Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+
+# -----------------------------------------------------------------
+
+#       7
+#      / \
+#     5   8
+#    / \   \
+#   1   6   9
+#  / \   \
+# 0   3   4
+#      \
+#       2
+
+# Root 7'dir.
+# Root'un solunda 5 bulunur.
+# Root'un sağında 8 bulunur.
+# 5'in solunda 1 bulunur.
+# 5'in sağında 6 bulunur.
+# 8'in sağında 9 bulunur.
+# 1'in solunda 0 bulunur.
+# 1'in sağında 3 bulunur.
+# 6'in sağında 4 bulunur.
+# 3'in sağında 2 bulunur.
